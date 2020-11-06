cc7e5b0734919e068fea3897d902273998a9c05b
Laurent147 <laurent.burte@gmail.com>
2020-11-05 20:39:18 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+      formidable = require('formidable'),
+      path = require('path'),
+      app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('index.html')
+})
+
+app.listen(3000, '192.168.0.46', (err, stat) => {
+  console.log((err? err : "server listening on 192.168.0.46:3000"));
+})
